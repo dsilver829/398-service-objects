fb7fc867dd05cb8e79e3bc0e430d49b4d5797c31
David Silver <dsilver@candidatemetrics.com>
2013-02-09 10:56:11 -0800

    setting up

--- auth-services/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ActiveRecord::Base
-  attr_accessible :email, :username, :password, :password_confirmation
-
-  validates_presence_of :username, :email
-  validates_uniqueness_of :username
-  validates_confirmation_of :password
-
-  has_secure_password
-end
